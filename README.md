8c807dc5f40089af4b0c37a8c3b9a5781eaa2df9
Anjey Tsibylskij <anjey.tsibylskij@gmail.com>
2016-02-16 14:25:44 +0200

    Add AMD support

---
+++ README.md
@@ -0,0 +1,176 @@
+# Browser ID3 Writer
+
+[![npm](https://img.shields.io/npm/v/browser-id3-writer.svg?style=flat-square)](https://www.npmjs.com/package/browser-id3-writer)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg?style=flat-square)](https://github.com/semantic-release/semantic-release)
+[![npm](https://img.shields.io/npm/l/browser-id3-writer.svg?style=flat-square)](https://github.com/egoroof/browser-id3-writer/blob/master/LICENSE.md)
+
+[![Travis](https://img.shields.io/travis/egoroof/browser-id3-writer.svg?style=flat-square)](https://travis-ci.org/egoroof/browser-id3-writer)
+[![Code Climate](https://img.shields.io/codeclimate/coverage/github/egoroof/browser-id3-writer.svg?style=flat-square)](https://codeclimate.com/github/egoroof/browser-id3-writer/coverage)
+[![Code Climate](https://img.shields.io/codeclimate/github/egoroof/browser-id3-writer.svg?style=flat-square)](https://codeclimate.com/github/egoroof/browser-id3-writer)
+
+[![David](https://img.shields.io/david/egoroof/browser-id3-writer.svg?style=flat-square)](https://david-dm.org/egoroof/browser-id3-writer)
+[![David](https://img.shields.io/david/dev/egoroof/browser-id3-writer.svg?style=flat-square)](https://david-dm.org/egoroof/browser-id3-writer#info=devDependencies)
+
+This is a library for writing [ID3v2.3](http://id3.org/id3v2.3.0)
+tag to audio files in browsers.
+It can not read tag so use another lib to do it.
+
+**Note**: the library removes existing ID3 tag (v2.2, v2.3 and v2.4).
+
+## Browser requirements
+- [ArrayBuffer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer)
+- [Uint8Array](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array)
+- [Blob](https://developer.mozilla.org/en-US/docs/Web/API/Blob)
+- [URL](https://developer.mozilla.org/en-US/docs/Web/API/URL)
+- [TextEncoder](https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder)
+
+## Demo
+
+Demonstration is available here: [egoroof.github.io/browser-id3-writer](https://egoroof.github.io/browser-id3-writer/)
+
+## Installation
+
+You can include library via [npmcdn](https://npmcdn.com/):
+
+```html
+<script src="//npmcdn.com/browser-id3-writer@1.3.0/dist/browser-id3-writer.min.js"></script>
+```
+
+Or you can install via [npm](https://www.npmjs.com/) and get it from `dist` folder:
+
+```
+npm install browser-id3-writer
+```
+
+Or you can include it using browser module loaders like webpack or browserify:
+
+```js
+var ID3Writer = require('browser-id3-writer');
+```
+
+## Usage
+
+You should first get **arrayBuffer** of the song you would like to add ID3 tag.
+
+For example you can create file input and use `FileReader`:
+
+```html
+<input type="file" id="file" accept="audio/mpeg">
+<script>
+    document.getElementById('file').addEventListener('change', function () {
+        if (!this.files.length) {
+            return;
+        }
+        var reader = new FileReader();
+        reader.onload = function () {
+            var arrayBuffer = reader.result;
+            // go next
+        };
+        reader.onerror = function () {
+            // handle error
+            console.error('Reader error', reader.error);
+        };
+        reader.readAsArrayBuffer(this.files[0]);
+    });
+</script>
+```
+
+To get arrayBuffer from remote server you can use `XMLHttpRequest`:
+
+```js
+var xhr = new XMLHttpRequest();
+xhr.open('GET', urlToSongFile, true);
+xhr.responseType = 'arraybuffer';
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        var arrayBuffer = xhr.response;
+        // go next
+    } else {
+        // handle error
+        console.error(xhr.statusText + ' (' + xhr.status + ')');
+    }
+};
+xhr.onerror = function() {
+    // handle error
+    console.error('Network error');
+}
+xhr.send();
+```
+
+Then init `ID3Writer` object, set frames and add a tag:
+
+```js
+var writer = new ID3Writer(arrayBuffer);
+writer.setFrame('TIT2', 'Home')
+    .setFrame('TPE1', ['Eminem', '50 Cent'])
+    .setFrame('TPE2', 'Eminem')
+    .setFrame('TALB', 'Friday Night Lights')
+    .setFrame('TYER', 2004)
+    .setFrame('TRCK', '6/8')
+    .setFrame('TPOS', '1/2')
+    .setFrame('TCON', ['Soundtrack'])
+    .setFrame('USLT', 'This is unsychronised lyrics')
+    .setFrame('APIC', coverArrayBuffer);
+writer.addTag();
+
+// now you can save it to file as you wish
+var arrayBuffer = writer.arrayBuffer;
+var blob = writer.getBlob();
+var url = writer.getURL();
+```
+
+For example you can save file using [FileSaver.js](https://github.com/eligrey/FileSaver.js/):
+
+```js
+saveAs(blob, 'song with tags.mp3');
+```
+
+If you are writing chromium extension you can save file using
+[Downloads API](https://developer.chrome.com/extensions/downloads):
+
+```js
+chrome.downloads.download({
+    url: url,
+    filename: 'song with tags.mp3'
+});
+```
+
+## Memory control
+
+When you generate URLs via `writer.getURL()` you should know
+that whole file is kept in memory until you close the page or move to another one.
+So if you generate lots of URLs in a single page you should manually free memory
+after you finish downloading file:
+
+```js
+URL.revokeObjectURL(url); // if you know url or
+writer.revokeURL(); // if you have access to writer
+```
+
+## Supported frames
+
+Currently you can set next frames:
+
+**array of strings:**
+
+- TPE1 (song artists)
+- TCOM (song composers)
+- TCON (song genre)
+
+**string**
+
+- TIT2 (song title)
+- TALB (album title)
+- TPE2 (album artist)
+- TRCK (song number in album): 5 or '5/10'
+- TPOS (album disc number): 1 or '1/3'
+- USLT (unsychronised lyrics)
+
+**integer**
+
+- TLEN (song duration in milliseconds)
+- TYER (album release year)
+
+**arrayBuffer**
+
+- APIC (song cover): works with jpeg, png, gif, webp, tiff, bmp and ico
---
+++ test/browser-id3-writer.test.js
@@ -0,0 +1,223 @@
+var assetsPath = '/base/test/assets/';
+
+function ajax(url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            onSuccess(xhr.response);
+        } else {
+            onError(new Error(xhr.statusText + ' (' + xhr.status + ')'));
+        }
+    };
+    xhr.onerror = function () {
+        onError(new Error('Network error'));
+    };
+    xhr.send();
+}
+
+function typedArray2Array(typedArray) {
+    return Array.prototype.slice.call(typedArray);
+}
+
+describe('ID3Writer', function () {
+
+    it('should be possible to create an instance', function () {
+        var buffer = new ArrayBuffer(0);
+        var writer = new ID3Writer(buffer);
+        expect(writer).to.be.instanceof(ID3Writer);
+    });
+
+    it('should throw an exception if no argument passed to constructor', function () {
+        expect(function () {
+            var writer = new ID3Writer();
+        }).to.throw(Error, 'First argument should be an instance of ArrayBuffer');
+    });
+
+    it('should change byte count', function (done) {
+        var songByteCount = 1141027;
+        var songWithTagByteCount = 1145201;
+        ajax(assetsPath + 'the_decant_session.mp3', function (arrayBuffer) {
+            expect(arrayBuffer.byteLength).to.be.equal(songByteCount);
+            var writer = new ID3Writer(arrayBuffer);
+            writer.setFrame('TIT2', 'song name')
+                .setFrame('TPE1', ['song artists']);
+            var arrayBufferWithTag = writer.addTag();
+            expect(arrayBufferWithTag.byteLength).to.be.equal(songWithTagByteCount);
+            done();
+        });
+    });
+
+    it('wrong frame value type should throw an exception', function () {
+        var frames = ['TPE1', 'TCOM', 'TCON'];
+        var buffer = new ArrayBuffer(0);
+        var writer = new ID3Writer(buffer);
+        frames.forEach(function (frameName) {
+            expect(function () {
+                writer.setFrame(frameName, '');
+            }).to.throw(Error, 'frame value should be an array of strings');
+            expect(writer.setFrame(frameName, [])).to.be.instanceof(ID3Writer);
+        });
+    });
+
+    it('set incorrect frame name should throw an exception', function () {
+        var buffer = new ArrayBuffer(0);
+        var writer = new ID3Writer(buffer);
+        expect(function () {
+            writer.setFrame('wrongFrameName', 'val');
+        }).to.throw(Error, 'Unsupported frame');
+    });
+
+    it('should set USLT frame', function () {
+        var lyrics = 'Вышел заяц на крыльцо. Rabbit went out.';
+        var writer = new ID3Writer(new ArrayBuffer(0));
+        writer.setFrame('USLT', lyrics);
+        var buffer = writer.addTag();
+
+        var coder16 = new TextEncoder('utf-16le');
+        var frameTotalSize = lyrics.length * 2 + 16;
+        var bufferUint8 = new Uint8Array(buffer, 10, frameTotalSize);
+
+        expect(bufferUint8).to.eql(new Uint8Array([
+                85, 83, 76, 84, // 'USLT'
+                0, 0, 0, frameTotalSize - 10, // size without header (should be less than 128)
+                0, 0, // flags
+                1, // encoding
+                101, 110, 103, // language
+                0, 0 // content descriptor
+            ].concat(typedArray2Array(coder16.encode(lyrics)))
+        ));
+    });
+
+    describe('APIC', function () {
+
+        it('should throw error when mime type is not detected', function () {
+            var writer = new ID3Writer(new ArrayBuffer(0));
+            expect(function () {
+                writer.setFrame('APIC', new ArrayBuffer(20));
+            }).to.throw(Error, 'Unknown picture MIME type');
+        });
+
+        it('should throw error when buffer is empty', function () {
+            var writer = new ID3Writer(new ArrayBuffer(0));
+            expect(function () {
+                writer.setFrame('APIC', new ArrayBuffer(0));
+            }).to.throw(Error, 'Unknown picture MIME type');
+        });
+
+        it('should accept various image types', function () {
+            var types = [
+                { // jpeg
+                    signature: [0xff, 0xd8, 0xff],
+                    mime: [105, 109, 97, 103, 101, 47, 106, 112, 101, 103]
+                },
+                { // png
+                    signature: [0x89, 0x50, 0x4e, 0x47],
+                    mime: [105, 109, 97, 103, 101, 47, 112, 110, 103]
+                },
+                { // gif
+                    signature: [0x47, 0x49, 0x46],
+                    mime: [105, 109, 97, 103, 101, 47, 103, 105, 102]
+                },
+                { // webp
+                    signature: [0, 0, 0, 0, 0, 0, 0, 0, 0x57, 0x45, 0x42, 0x50],
+                    mime: [105, 109, 97, 103, 101, 47, 119, 101, 98, 112]
+                },
+                { // tiff LE
+                    signature: [0x49, 0x49, 0x2a, 0],
+                    mime: [105, 109, 97, 103, 101, 47, 116, 105, 102, 102]
+                },
+                { // tiff BE
+                    signature: [0x4d, 0x4d, 0, 0x2a],
+                    mime: [105, 109, 97, 103, 101, 47, 116, 105, 102, 102]
+                },
+                { // bmp
+                    signature: [0x42, 0x4d],
+                    mime: [105, 109, 97, 103, 101, 47, 98, 109, 112]
+                },
+                { // ico
+                    signature: [0, 0, 1, 0],
+                    mime: [105, 109, 97, 103, 101, 47, 120, 45, 105, 99, 111, 110]
+                }
+            ];
+
+            var content = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            types.forEach(function (type) {
+                var coverBuffer = new ArrayBuffer(type.signature.length + content.length);
+                var coverUint8 = new Uint8Array(coverBuffer);
+                coverUint8.set(type.signature);
+                coverUint8.set(content, type.signature.length);
+
+                var writer = new ID3Writer(new ArrayBuffer(0));
+                writer.setFrame('APIC', coverBuffer);
+                var buffer = writer.addTag();
+                var frameTotalSize = type.mime.length + type.signature.length + content.length + 14;
+                var bufferUint8 = new Uint8Array(buffer, 10, frameTotalSize);
+
+                expect(bufferUint8).to.eql(new Uint8Array([
+                        65, 80, 73, 67, // 'APIC'
+                        0, 0, 0, frameTotalSize - 10, // size without header (should be less than 128)
+                        0, 0, // flags
+                        0 // encoding
+                    ].concat(type.mime)
+                    .concat([0, 3, 0]) // delemiter, pic type, delemiter
+                    .concat(type.signature)
+                    .concat(content)
+                ));
+            });
+        });
+
+    });
+
+    describe('musicmetadata', function () {
+
+        it('should return an error if there is no tag', function (done) {
+            ajax(assetsPath + 'the_decant_session.mp3', function (arrayBuffer) {
+                var writer = new ID3Writer(arrayBuffer);
+                musicmetadata(writer.getBlob(), function (err, res) {
+                    expect(err).to.be.instanceof(Error);
+                    expect(err.message).to.be.equal('Could not find metadata header');
+                    done();
+                });
+            });
+        });
+
+        it('reading frames should give the same result as writing', function (done) {
+            ajax(assetsPath + 'the_decant_session.mp3', function (songArrayBuffer) {
+                ajax(assetsPath + 'cover.jpg', function (coverArrayBuffer) {
+                    var writer = new ID3Writer(songArrayBuffer);
+                    writer.setFrame('TIT2', 'Home')
+                        .setFrame('TPE1', ['Eminem', '50 Cent'])
+                        .setFrame('TPE2', 'Eminem')
+                        .setFrame('TALB', 'Friday Night Lights')
+                        .setFrame('TYER', 2004)
+                        .setFrame('TRCK', '6/8')
+                        .setFrame('TPOS', '1/2')
+                        .setFrame('TCON', ['Soundtrack'])
+                        .setFrame('APIC', coverArrayBuffer);
+                    writer.addTag();
+                    writer.addTag(); // test removing existing frame
+                    musicmetadata(writer.getBlob(), function (err, tag) {
+                        expect(err).to.be.an('undefined');
+                        expect(tag.title).to.be.equal('Home');
+                        expect(tag.artist[0]).to.be.equal('Eminem/50 Cent'); // https://github.com/leetreveil/musicmetadata/issues/105
+                        expect(tag.albumartist[0]).to.be.equal('Eminem');
+                        expect(tag.album).to.be.equal('Friday Night Lights');
+                        expect(tag.year).to.be.equal('2004');
+                        expect(tag.track.no).to.be.equal(6);
+                        expect(tag.track.of).to.be.equal(8);
+                        expect(tag.disk.no).to.be.equal(1);
+                        expect(tag.disk.of).to.be.equal(2);
+                        expect(tag.genre[0]).to.be.equal('Soundtrack');
+                        expect(tag.picture[0].format).to.be.equal('jpg');
+                        expect(tag.picture[0].data.length).to.be.equal(7168);
+                        done();
+                    });
+                });
+            });
+        });
+
+    });
+
+});
